2e1cc62052a3bda3d1eeef7a4e78024ac0a30942
rafamuniz <rafamuniz@gmail.com>
2016-05-19 01:12:35 +1000

    new Icons

--- Views/Web/obj/Release/Package/PackageTmp/Scripts/app/sensor.js
+++ Views/Web/obj/Release/Package/PackageTmp/Scripts/app/sensor.js
@@ -1,4 +1,28 @@
-﻿
+﻿function GetsTankBySite(siteId, element, url) {
+    if (siteId != "" && siteId != "0") {
+        var procemessage = "<option value='0'> Please wait...</option>";
+        $("#" + element).html(procemessage).show();
+
+        $.ajax({
+            url: url,
+            data: { siteId: siteId },
+            cache: false,
+            type: "GET",
+            success: function (data) {
+                var markup = "<option value=''>-- Please select a Tank -- </option>";
+                for (var x = 0; x < data.length; x++) {
+                    markup += "<option value=" + data[x].Value + ">" + data[x].Text + "</option>";
+                }
+                $("#" + element).html(markup).show();
+                $("#" + element).removeAttr("disabled");
+            },
+            error: function (reponse) {
+                notifiyError("error : " + reponse);
+            }
+        });
+    }
+};
+
 function getsItemBySensorTypeId(items) {
     var val = $("#ddlSensorType").val();
 
